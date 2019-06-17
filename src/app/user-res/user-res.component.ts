d416cb945c300bac5365ca82b2a02db54577c86d
xDaniil <dan.mednikov@icloud.com>
2019-06-17 23:07:05 +0300

    hui

--- src/app/user-res/user-res.component.ts
+++ src/app/user-res/user-res.component.ts
@@ -12,10 +12,14 @@ import { getUser } from '../store/selectors/user.selectors';
 })
 export class UserResComponent {
 
+  condition: any;
   formState$: Observable<IUserState>;
 
   constructor(private _store: Store<IAppState>) {
     this.formState$ = _store.select(getUser);
+    this.condition = (parseInt(this.formState$.toString()) >= 0);
+    console.log(this.condition);
+    console.log(this.formState$.toString());
   }
 
 }
